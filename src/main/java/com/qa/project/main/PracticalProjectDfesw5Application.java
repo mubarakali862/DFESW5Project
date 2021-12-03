102effac8b3a3ebbcacb9c02716c7e843d750ec6
mubarakali521 <mubarakomarali@gmail.com>
2021-12-03 13:49:17 +0000

    first commit

---
+++ src/main/java/com/qa/project/main/PracticalProjectDfesw5Application.java
@@ -0,0 +1,13 @@
+package com.qa.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalProjectDfesw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalProjectDfesw5Application.class, args);
+	}
+
+}
